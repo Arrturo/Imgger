c839a65ef7b703eacafdeb916b039d58849ddcff
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-03-07 13:38:33 +0000

    first mutations and a little corrections in model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post, Category, Image, Comment, Subcomment
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Subcomment)
--- backend/core/models.py
+++ backend/core/models.py
@@ -2,21 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-
-class Post(models.Model):
-    title = models.CharField(max_length=25, null=False)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
-    description = models.TextField(max_length=255, null=True, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    image_id = models.ForeignKey('Image', on_delete=models.CASCADE)
-    category_id = models.ForeignKey(
-        'Category', null=False, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.title}"
-
+import graphene
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -24,26 +10,38 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-
 class Image(models.Model):
     file = models.ImageField()
+    
 
+class Post(models.Model):
+    title = models.CharField(max_length=25, null=False)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
 
 class Comment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=2555)
     create_time = models.DateTimeField(auto_now_add=True)
-    post_id = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.comment[:20]}"
 
-
 class Subcomment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment_id = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     content = models.CharField(max_length=2555)
     create_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.content[:20]}"
+        return f"{self.comment[:20]}"
