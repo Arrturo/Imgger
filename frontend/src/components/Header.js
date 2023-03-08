abc39697e7616a8f52b38236e738912b09785dc9
KrystianKierklo <krystian.kierklo3@gmail.com>
2023-03-08 09:19:35 +0100

    Header and footer add

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../index.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {useDispatch, useSelector} from 'react-redux'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap'
+
+
+function Header() {
+	
+
+	return (
+        <header className="text-2xl">
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container fluid >
+                    <LinkContainer to='/'>
+					    <Navbar.Brand className="hover:text-amber-400 hover:font-bold text-2xl" href='/'>PySquad</Navbar.Brand>
+                    </LinkContainer>
+					<Navbar.Toggle aria-controls='navbarScroll' />
+					<Navbar.Collapse id='navbarScroll'>
+						<Nav className='my-2 my-lg-0 ms-auto p-1 gap-5' navbarScroll>
+                            <LinkContainer to='/'>
+                                <Nav.Link className="hover:text-amber-400"> <i class="fa-solid fa-plus"></i> Dodaj zdjęcie </Nav.Link>
+                            </LinkContainer>
+							<LinkContainer to='/'>
+								<Nav.Link className="hover:text-amber-400"><i class="fa-solid fa-right-to-bracket"></i> Zaloguj się</	Nav.Link>
+							</LinkContainer>														
+							<LinkContainer to='/'>
+								<Nav.Link className="hover:text-amber-400"> Rejestracja</	Nav.Link>
+							</LinkContainer>														
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+      );
+}
+
+export default Header;
