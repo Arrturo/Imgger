ac7184a8c423213a3146de7b8e0a9db764e65984
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-03-09 11:38:57 +0000

    add next mutations

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -245,12 +245,64 @@ class Query(graphene.ObjectType):
 
     def resolve_images(self, info, **kwargs):
         return Image.objects.all()
+class UpdateCategoryMutation(graphene.Mutation):
+    class Arguments:
+        category_id = graphene.ID(required=True)
+        name = graphene.String()
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, category_id, name):
+        category = Category.objects.get(id=category_id)
+        if name:
+            category.name = name
+        category.save()
+        return UpdateCategoryMutation(category=category)
+
+class DeleteCategoryMutation(graphene.Mutation):
+    class Arguments:
+        category_id = graphene.ID(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, category_id):
+        category = Category.objects.get(id=category_id)
+        category.delete()
+        return DeleteCategoryMutation(category=category)
+
+class Query(graphene.ObjectType):
+    users = graphene.List(UserType)
+    posts = graphene.List(PostType)
+    categories = graphene.List(CategoryType)
+    images = graphene.List(ImageType)
+    comments = graphene.List(CommentType)
+    subcomments = graphene.List(SubcommentType)
+
+    def resolve_users(self, info, **kwargs):
+        return User.objects.all()
+
+    def resolve_posts(self, info, **kwargs):
+        return Post.objects.all()
+
+    def resolve_categories(self, info, **kwargs):
+        return Category.objects.all()
+
+    def resolve_comments(self, info, **kwargs):
+        return Comment.objects.all()
+
+    def resolve_subcomments(self, info, **kwargs):
+        return Subcomment.objects.all()
+
+    def resolve_images(self, info, **kwargs):
+        return Image.objects.all()
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUserMutation.Field()
     update_user = UpdateUserMutation.Field()
     delete_user = DeleteUserMutation.Field()
     create_post = CreatePostMutation.Field()
+    update_post = UpdatePostMutation.Field()
+    delete_post = DeletePostMutation.Field()
     update_post = UpdatePostMutation.Field()
     delete_post = DeletePostMutation.Field()
     create_category = CreateCategoryMutation.Field()
