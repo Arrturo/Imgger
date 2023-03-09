23b4823c8a57734677533e70ad4ddc4f209be697
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-03-09 11:38:57 +0000

    add next mutations

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -49,6 +49,56 @@ class AuthMutatuion(graphene.ObjectType):
     password_reset = mutations.PasswordReset.Field()
     password_change = mutations.PasswordChange.Field()
 
+class UpdateCategoryMutation(graphene.Mutation):
+    class Arguments:
+        category_id = graphene.ID(required=True)
+        name = graphene.String()
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, category_id, name):
+        category = Category.objects.get(id=category_id)
+        if name:
+            category.name = name
+        category.save()
+        return UpdateCategoryMutation(category=category)
+
+class DeleteCategoryMutation(graphene.Mutation):
+    class Arguments:
+        category_id = graphene.ID(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, category_id):
+        category = Category.objects.get(id=category_id)
+        category.delete()
+        return DeleteCategoryMutation(category=category)
+
+class Query(graphene.ObjectType):
+    users = graphene.List(UserType)
+    posts = graphene.List(PostType)
+    categories = graphene.List(CategoryType)
+    images = graphene.List(ImageType)
+    comments = graphene.List(CommentType)
+    subcomments = graphene.List(SubcommentType)
+
+    def resolve_users(self, info, **kwargs):
+        return User.objects.all()
+
+    def resolve_posts(self, info, **kwargs):
+        return Post.objects.all()
+
+    def resolve_categories(self, info, **kwargs):
+        return Category.objects.all()
+
+    def resolve_comments(self, info, **kwargs):
+        return Comment.objects.all()
+
+    def resolve_subcomments(self, info, **kwargs):
+        return Subcomment.objects.all()
+
+    def resolve_images(self, info, **kwargs):
+        return Image.objects.all()
 
 class Mutation(AuthMutatuion, graphene.ObjectType):
     create_user = CreateUserMutation.Field()
