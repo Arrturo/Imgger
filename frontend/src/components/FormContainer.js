66aa92504abd8619eddb2e3277f51cb6a9c47051
KrystianKierklo <krystian.kierklo3@gmail.com>
2023-03-12 21:13:32 +0100

    Adding login and register screens

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+
+function FormContainer({children}) {
+  return (
+    <Container className="py-4 rounded-2xl border-4 border-rose-400 formularz max-w-screen-md mb-5">
+        <Row className="justify-content-center ">
+            <Col xs={12} md={6} >
+                {children}
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+
+export default FormContainer
