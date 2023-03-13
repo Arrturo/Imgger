4fbbe0966181bf182e86d251d5aa8897db292339
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-03-13 15:38:36 +0100

    add missing crud mutations

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,7 +13,6 @@ from django.contrib.auth.models import User, AbstractUser
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-
     def __str__(self):
         return f"{self.name}"
 
