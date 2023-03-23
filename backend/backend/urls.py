b2388af65d522ffb48d9a5abae7b746bd7baa6ed
aquadros1003 <jankowski.lukasz0987@gmail.com>
2023-03-23 11:11:09 +0100

    form to upload images
    Co-authored-by: Artur <Arrturo@users.noreply.github.com>

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,11 +21,13 @@ from graphene_file_upload.django import FileUploadGraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import image_upload_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True, schema=schema))),
+    path('upload/', image_upload_view, name='upload'),
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import UploadForm
 
 # Create your views here.
+
+def image_upload_view(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UploadForm()
+    return render(request, 'upload.html', {'form': form})
