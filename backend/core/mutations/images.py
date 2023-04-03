d8100504e413a1b743b7e8e8dcfdc9149cc06aa6
Jankowski Łukasz <jankowski.lukasz0987@gmail.com>
2023-04-03 22:04:11 +0200

    upload images using graphql and change email template

--- backend/core/mutations/images.py
+++ backend/core/mutations/images.py
@@ -1,97 +1,22 @@
 import graphene
 from ..models import Image
+from ..types import ImageType
 from graphene_file_upload.scalars import Upload
-from graphql_jwt.decorators import login_required
 
 
 class CreateImageMutation(graphene.Mutation):
     class Arguments:
-        input = Upload(required=True)
+        image = Upload(required=True)
 
     success = graphene.Boolean()
+    image = graphene.Field(ImageType)
     errors = graphene.String()
 
     @staticmethod
-    def mutate(root, info, input):
+    def mutate(root, info, image):
         try:
-            image = Image(name=input.name, image=input.image)
-            image.save()
-            success = True
-            errors = None
+            file = image
+            image = Image.objects.create(file=file)
+            return CreateImageMutation(success=True, image=image)
         except Exception as e:
-            success = False
-            errors = str(e)
-        return CreateImageMutation(success=success, errors=errors)
-
-
-class UpdateImageMutation(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-        input = Upload(required=True)
-
-    success = graphene.Boolean()
-    errors = graphene.String()
-
-    @staticmethod
-    def mutate(root, info, id, input):
-        try:
-            image = Image.objects.get(pk=id)
-            image.image = input.image
-            image.save()
-            success = True
-            errors = None
-        except Exception as e:
-            success = False
-            errors = str(e)
-        return UpdateImageMutation(success=success, errors=errors)
-
-
-class DeleteImageMutation(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-
-    success = graphene.Boolean()
-    errors = graphene.String()
-
-    @staticmethod
-    def mutate(root, info, id):
-        try:
-            image = Image.objects.get(pk=id)
-            image.delete()
-            success = True
-            errors = None
-        except Exception as e:
-            success = False
-            errors = str(e)
-        return DeleteImageMutation(success=success, errors=errors)
-
-
-class UpdateImageMutation(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-        input = Upload(required=True)
-
-    success = graphene.Boolean()
-    errors = graphene.String()
-
-    @staticmethod
-    @login_required
-    def mutate(root, info, id, input):
-        image = input.image
-        Image.objects.filter(id=id).update(image=image)
-        ok = True
-        return UpdateImageMutation(ok=ok)
-
-
-class DeleteImageMutation(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-
-    ok = graphene.Boolean()
-
-    @staticmethod
-    @login_required
-    def mutate(root, info, id):
-        Image.objects.filter(id=id).delete()
-        ok = True
-        return DeleteImageMutation(ok=ok)
+            return CreateImageMutation(success=False, errors=str(e))
